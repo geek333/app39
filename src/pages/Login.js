3eb2604d2cfb93d4acdf0910e01d492b4094381a
Prashant Sethi <psethi@saba.com>
2019-09-16 21:29:15 +0530

    login screen

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar
+} from 'react-native';
+
+export default class Login extends Component<{}>{
+    render( {
+        return(
+            <View>
+
+            </View>
+        }
+    })
+}
