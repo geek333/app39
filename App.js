06b9eb6f443579a6b51b8228bd4549537fe14ed3
Rajesh Kumar <cyber.geek205@gmail.com>
2019-09-17 01:29:34 +0530

    adding login screen and registration

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+import RegForm from './app/components/RegForm';
+
+
+
+export default class app extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <RegForm/>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:"center",
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding:15,
   },
 });
---
+++ app/components/RegForm.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { StyleSheet,
+    Text, 
+    View,
+    TextInput,
+    TouchableOpacity, } from 'react-native';
+
+import {Actions} from 'react-native-router-flux';
+import Router from './Routers'
+
+export default class RegForm extends React.Component {
+
+  signIn()
+  {
+    Actions.login();
+  }
+    render(){
+
+        return (
+            <View style={styles.RegForm}>
+               <Text style={styles.header}>Registration</Text>
+               
+               <TextInput style={styles.textinput} underlineColorAndroid={'transparent'} placeholder="Your Name" />
+               
+               <TextInput style={styles.textinput} underlineColorAndroid={'transparent'} placeholder="Your Email" />
+             
+               <TextInput style={styles.textinput} secureTextEntry={true} underlineColorAndroid={'transparent'} placeholder="Password" />
+             
+               <TextInput style={styles.textinput} secureTextEntry={true} underlineColorAndroid={'transparent'} placeholder="Confirm Password" />
+             
+              <TouchableOpacity style={styles.button}>
+                  <Text style={styles.btntext}>Sign Up</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.button}>
+                  <Text style={styles.btntext} onPress={this.signIn}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+          );
+    }
+  
+}
+
+const styles = StyleSheet.create({
+  RegForm: {
+    alignSelf : 'stretch',
+  },
+  header:{
+      fontSize :24,
+      color : '#59cbbd',
+      paddingBottom : 10,
+      marginBottom : 40,
+      borderBottomColor : '#199187',
+      borderBottomWidth: 1,
+  },
+  textinput :{
+      alignSelf: "stretch",
+      height:40,
+      marginBottom:30,
+      color:'#fff',
+      borderBottomColor:'#59cbbd',
+      borderBottomWidth:1,
+  },
+  button:
+  {
+    alignSelf:"stretch",
+    alignItems:"center",
+    padding:20,
+    backgroundColor:'#59cbbd',
+    marginTop:30,
+  },
+  btntext:
+  {
+    color:'#fff',
+    fontWeight:"bold",
+  }
+});
